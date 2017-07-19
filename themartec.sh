86cf3c52be2a51662817781ed3924fdbf26b6cc5
Frederico Martini <fredmalmeida@gmail.com>
2017-07-19 20:39:37 -0300

    added script to turn on themartec environment

---
+++ themartec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#turn on containers
+./start-environment start
+
+#cp virtualhost
+docker cp config/apache2/sites-enabled/themartec.conf apache2-php:/etc/apache2/sites-enabled/
+
+#replace php.ini
+docker exec apache2-php sed -i "s,error_reporting =.*,error_reporting = E_ALL \& ~E_NOTICE,g" /etc/php/7.0/apache2/php.ini
+
+#restart service
+docker exec  apache2-php service apache2 reload
+
